98c2c98c5f330f1c80ae782d521aa405ad31baed
muhammad hafizh <mhafizh1201@gmail.com>
2023-05-21 12:10:09 +0700

    the newest

---
+++ script.js
@@ -0,0 +1,32 @@
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
+function hamProfilDropdown() {
+    var y = document.getElementById("forProfil");
+    if (y.style.display === "block") {
+      y.style.display = "none";
+    } else {
+      y.style.display = "block";
+    }
+}
+function hamGaleryDropdown() {
+    var y = document.getElementById("forGalery");
+    if (y.style.display === "block") {
+      y.style.display = "none";
+    } else {
+      y.style.display = "block";
+    }
+}
+function LabDropdown() {
+    var y = document.getElementById("forLab");
+    if (y.style.display === "block") {
+      y.style.display = "none";
+    } else {
+      y.style.display = "block";
+    }
+}
